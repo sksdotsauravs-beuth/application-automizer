ec8d642668d53ed08727f859a4bbefbe1d097fbc
sksdotsauravs-beuth <s81481@beuth-hochschule.de>
2021-02-16 03:14:41 +0100

    - removed unused source files

--- source/utils/selenium_utils.py
+++
@@ -1,8 +0,0 @@
-class SeleniumUtils:
-    """
-        - author:             Saurav Kumar Saha
-        - created:            2021-02-07
-        - changed:            2020-02-07
-
-        This class will provide certain selenium driver functionalities.
-    """
